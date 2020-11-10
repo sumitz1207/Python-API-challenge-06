ecc985f93a996e49a5b2bbdb0770991adaf484d0
Sumit Patel <sumitz1207@gmail.com>
2020-11-10 10:19:31 -0500

    update everything to include titles

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "3efc7aac8749d590cdb362dfe84458e2"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyChW7HxR18lAimHaftQmNwD-qk8rMLzTg8"
